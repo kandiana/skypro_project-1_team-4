34e3279722b11aa6bdddd72dc1538528aa52bfcf
AlexRaevskiy <alexraevskii82@gmail.com>
2021-09-29 12:16:15 +0300

    Creating render login screen

--- assets/js/screen_login.js
+++ assets/js/screen_login.js
@@ -1,30 +1,66 @@
-function renderLoginScreen() {
-  const app = document.querySelector('.app');
-  app.textContent = '';
+// window.application.blocks['input'] = renderInput;
+// window.application.blocks['login-button'] = renderloginButton;
+// window.application.screens['auth'] = renderAuthScreen;
 
-  const title = document.createElement('h1');
-  title.textContent = 'Камень, ножницы, бумага';
+// function renderInput(container) {
+//   const input = document.querySelector('input');
+//   input.classList.add('input');
+//   container.appendChild(input);
+// }
 
-  const content = document.createElement('div');
-  const login = document.createElement('h2');
-  login.textContent = 'Введите ваше имя';
-  const inputLogin = document.createElement('input');
-  const buttonLogin = document.createElement('button');
-  buttonLogin.textContent = 'Начать игру';
+// function renderloginButton(container) {
+//   const loginButton = document.querySelector('button');
+//   input.classList.add('button');
+//   loginButton.textContent = 'Начать играть';
+//   container.appendChild(loginButton);
+// }
 
-  app.appendChild(title);
-  app.appendChild(content);
-  content.appendChild(login);
-  content.appendChild(inputLogin);
-  content.appendChild(buttonLogin);
+// function renderAuthScreen() {
+//   app.textContent = '';
+//   window.application.renderBlock('input', app);
+//   window.application.renderBlock('login-button', app);
+// }
+// renderAuthScreen()
 
-  let enteredLogin;
 
-  buttonLogin.addEventListener('touchstart', function () {
-    enteredLogin = inputLogin.value;
-    console.log('touch');
-    console.log(enteredLogin);
-  });
-}
 
-renderLoginScreen();
+
+
+
+
+
+
+
+
+
+
+// function renderLoginScreen() {
+//   const app = document.querySelector('.app');
+//   app.textContent = '';
+
+//   const title = document.createElement('h1');
+//   title.textContent = 'Камень, ножницы, бумага';
+
+//   const content = document.createElement('div');
+//   const login = document.createElement('h2');
+//   login.textContent = 'Введите ваше имя';
+//   const inputLogin = document.createElement('input');
+//   const buttonLogin = document.createElement('button');
+//   buttonLogin.textContent = 'Начать игру';
+
+//   app.appendChild(title);
+//   app.appendChild(content);
+//   content.appendChild(login);
+//   content.appendChild(inputLogin);
+//   content.appendChild(buttonLogin);
+
+//   let enteredLogin;
+
+//   buttonLogin.addEventListener('touchstart', function () {
+//     enteredLogin = inputLogin.value;
+//     console.log('touch');
+//     console.log(enteredLogin);
+//   });
+// }
+
+// renderLoginScreen();
