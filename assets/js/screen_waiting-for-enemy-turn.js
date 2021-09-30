02825ad842c2778b2567adc10c27f56f980a3c24
slepovsde <slepovsde@gmail.com>
2021-09-30 17:29:01 +0700

    win, loose, wait move

---
+++ assets/js/screen_waiting-for-enemy-turn.js
@@ -0,0 +1,42 @@
+window.application.blocks['enemyMoveBlock'] = renderEnemyMoveBlock
+window.application.screens['enemyMoveScreen'] = renderEnemyMoveScreen
+
+function renderEnemyMoveBlock(container) {
+    const loader = document.createElement('h1')
+    loader.classList.add('loader')
+    container.appendChild(loader)
+    return loader;
+}
+
+function renderEnemyMoveScreen() {                              //отрисовка экрана
+    app.textContent = ""
+    
+    const loader = window.application.renderBlock('enemyMoveBlock')
+    loader.textContent = 'Ожидание хода соперника'
+}
+
+//  window.application.renderScreen('playScreen')
+// window.application.renderScreen('enemyMoveScreen')
+
+function recievedData(responseText) {
+         const data = JSON.parse(responseText)
+         console.log(data)
+
+switch (window.application.game.status) {
+    case 'waiting-for-enemy-move':
+        window.application.renderScreen('enemyMoveScreen')
+        break
+    case 'waiting-for-your-move':
+            window.application.renderScreen('playScreen')
+        break
+    case 'win':
+            window.application.renderScreen('winScreen')
+        break
+    case 'loose':
+            window.application.renderScreen('looseScreen')
+        break    
+}    
+
+const timer = setInterval(() => request('game-status', requestParameters, recievedData), 500)
+window.application.timers.push(timer)
+}
