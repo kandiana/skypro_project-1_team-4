00c8e9c31ed273a44cc9a69fcb70fa53dc81360d
Maria Babintseva <maria_ov4@mail.ru>
2021-09-30 13:32:21 +0300

    created screen_move

---
+++ assets/js/screen_move.js
@@ -0,0 +1,99 @@
+window.application.blocks['screenTitle'] = renderScreenTitle;
+window.application.blocks['moveButton'] = renderButton;
+window.application.blocks['playBlock'] = renderPlayBlock;
+window.application.screens['playScreen'] = renderPlayScreen;
+
+//Функция отрисовки заголовка
+
+function renderScreenTitle(container) {
+    const screenTitle = document.createElement('h1')
+    screenTitle.classList.add('title')
+    container.appendChild(screenTitle)
+
+    return screenTitle
+}
+
+//Функция отрисовки кнопки
+function renderButton(container) {
+    const button = document.createElement('button')
+    button.classList.add('button')
+    container.appendChild(button)
+    return button
+}
+
+//Функция отрисовки блока игры
+function renderPlayBlock(container) {
+    //создаем контейнер для кнопок
+    const div = document.createElement('div')
+    div.classList.add('playBlock')
+    container.appendChild(div)
+
+    //отрисовка кнопки "Камень"
+    const rock = window.application.renderBlock('moveButton', div)
+    const data_name_rock = document.createAttribute('data-name')
+    rock.setAttributeNode(data_name_rock)
+    data_name_rock.value = 'rock'
+    rock.textContent = 'Камень'
+
+    //отрисовка кнопки "Ножницы"
+    const scissors = window.application.renderBlock('moveButton', div)
+    const data_name_scissors = document.createAttribute('data-name')
+    scissors.setAttributeNode(data_name_scissors)
+    data_name_scissors.value = 'scissors'
+    scissors.textContent = 'Ножницы'
+
+    //отрисовка кнопки "Бумага"
+
+    const paper = window.application.renderBlock('moveButton', div)
+    const data_name_paper = document.createAttribute('data-name')
+    paper.setAttributeNode(data_name_paper)
+    data_name_paper.value = 'paper'
+    paper.textContent = 'Бумага'
+
+    //По нажатию на кнопку отправляем запрос
+    div.addEventListener('touchend', function (e) {
+        console.log(e)
+        console.log(e.target.dataset.name)
+        //Параметры, необходимые для запроса
+        const requestParameters = {
+            token: window.application.player.token,
+            id: window.application.game.id,
+            move: `${e.target.dataset.name}`
+
+        }
+        console.log(requestParameters)
+        //Функция обработки полученных данных
+        function recievedData(responseText) {
+            const data = JSON.parse(responseText)
+            console.log(data)
+            switch (data['game-status'].status) {
+                case 'waiting-for-enemy-move':
+                    window.application.renderScreen('enemyMoveScreen')
+                    break;
+
+                case 'win':
+                    window.application.renderScreen('winScreen')
+                    break;
+
+                case 'loose':
+                    window.application.renderScreen('looseScreen')
+                    break;
+            }
+        }
+
+        request('play', requestParameters, recievedData)
+
+        return div;
+    })
+
+}
+
+//Функция отрисовки экрана
+function renderPlayScreen() {
+    const title = window.application.renderBlock('screenTitle', app)
+    title.textContent = 'Ход'
+    window.application.renderBlock('playBlock', app)
+}
+
+/*window.application.renderScreen('playScreen')*/
+
