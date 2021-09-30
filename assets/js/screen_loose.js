7ccb67a059a0c302248d157419f86935ba121754
slepovsde <slepovsde@gmail.com>
2021-09-30 23:19:34 +0700

    change correctives

--- assets/js/screen_loose.js
+++ assets/js/screen_loose.js
@@ -1,50 +1,54 @@
-window.application.blocks['looseBlock'] = renderLooseBlock
-window.application.blocks['playButton'] = renderPlayButton
-window.application.blocks['lobbyButton'] = renderLobbyButton
-window.application.screens['looseScreen'] = renderLooseScreen
+window.application.blocks["looseBlock"] = renderLooseBlock;
+window.application.blocks["playButton"] = renderPlayButton;
+window.application.blocks["lobbyButton"] = renderLobbyButton;
+window.application.screens["looseScreen"] = renderLooseScreen;
 
-function renderLooseBlock(container) {                          //создание инфомационного блока с текстом поражения
-  const looseBlock = document.createElement('div');
-  container.appendChild(looseBlock)
-  looseBlock.classList.add('loose-block');
-  
-  return looseBlock
+function renderLooseBlock(container) {
+  //создание инфомационного блока с текстом поражения
+  const looseBlock = document.createElement("div");
+  container.appendChild(looseBlock);
+  looseBlock.classList.add("loose-block");
+
+  return looseBlock;
 }
 
-function renderPlayButton(container) {                            //создание кнопки "Играть"
-  const playButton = document.createElement('button');
-  playButton.classList.add('button');
-  container.appendChild(playButton)
+function renderPlayButton(container) {
+  //создание кнопки "Играть"
+  const playButton = document.createElement("button");
+  playButton.classList.add("button");
+  container.appendChild(playButton);
 
-  playButton.addEventListener('touchend', startGame)
- 
-  return playButton
-}
-
-function renderLobbyButton(container) {                                         //создание кнопки "Перейти в лобби"
-  const lobbyButton = document.createElement('button');
-  lobbyButton.classList.add('button');
-  container.appendChild(lobbyButton)
-  
-
-  lobbyButton.addEventListener('touchend', () => {
-    window.application.renderScreen('lobbyScreen') 
+  playButton.addEventListener("touchend", () => {
+    window.application.renderScreen('playScreen');
   });
 
-  return lobbyButton  
+  return playButton;
 }
 
-function renderLooseScreen () {                                     //функция отрисовки экрана поражения
-  app.textContent = ''
-  
-  const looseBlock = window.application.renderBlock('looseBlock', app)
-	looseBlock.textContent = 'Вы проиграли!';
+function renderLobbyButton(container) {
+  //создание кнопки "Перейти в лобби"
+  const lobbyButton = document.createElement("button");
+  lobbyButton.classList.add("button");
+  container.appendChild(lobbyButton);
 
-	const lobbyButton = window.application.renderBlock('lobbyButton', app)
-  lobbyButton.textContent = 'Перейти в лобби';
+  lobbyButton.addEventListener("touchend", () => {
+    window.application.renderScreen("lobbyScreen");
+  });
 
-	const playButton = window.application.renderBlock('playButton', app)
-	playButton.textContent = 'Играть еще';
+  return lobbyButton;
+}
+
+function renderLooseScreen() {
+  //функция отрисовки экрана поражения
+
+  const looseBlock = window.application.renderBlock("looseBlock", app);
+  looseBlock.textContent = "Вы проиграли!";
+
+  const lobbyButton = window.application.renderBlock("lobbyButton", app);
+  lobbyButton.textContent = "Перейти в лобби";
+
+  const playButton = window.application.renderBlock("playButton", app);
+  playButton.textContent = "Играть еще";
 }
 
 //window.application.renderScreen('looseScreen');
